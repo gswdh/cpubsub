7f539ac55e46f4d495edffe22fc7c0188890eb29
George <georgewaller@gswdh.co.uk>
2024-12-23 13:44:21 +0000

    fix(pipe): pipe function implementations commented out

--- pipe.c
+++ pipe.c
@@ -5,25 +5,25 @@
 
 void pipe_set_length(pipe_t *pipe, uint32_t length) { pipe->length = length; }
 
-void __attribute__((weak)) pipe_init(pipe_t *pipe, uint32_t obj_size, uint32_t queue_len)
-{
-    assert(false && "Function not implemented.");
-}
+// void __attribute__((weak)) pipe_init(pipe_t *pipe, uint32_t obj_size, uint32_t queue_len)
+// {
+//     assert(false && "Function not implemented.");
+// }
 
-void __attribute__((weak)) pipe_delete(pipe_t *pipe)
-{
-    assert(false && "Function not implemented.");
-}
+// void __attribute__((weak)) pipe_delete(pipe_t *pipe)
+// {
+//     assert(false && "Function not implemented.");
+// }
 
-void __attribute__((weak)) pipe_push(pipe_t *pipe, void *p_item)
-{
-    assert(false && "Function not implemented.");
-}
+// void __attribute__((weak)) pipe_push(pipe_t *pipe, void *p_item)
+// {
+//     assert(false && "Function not implemented.");
+// }
 
-bool __attribute__((weak)) pipe_pop(pipe_t *pipe, void *p_item)
-{
-    assert(false && "Function not implemented.");
-    return false;
-}
+// bool __attribute__((weak)) pipe_pop(pipe_t *pipe, void *p_item)
+// {
+//     assert(false && "Function not implemented.");
+//     return false;
+// }
 
 uint32_t pipe_item_size(pipe_t *pipe) { return pipe->obj_size; }
